dac8c8d02e2755d23cd02faeee1166ec62bd0785
sam-pienaar-0101 <samkelompienaar@gmail.com>
2026-03-28 09:21:29 +0200

    Enhance product selection and checkout functionality
    Refactor product selection and checkout process for better user interaction and add functionality to handle multiple products.

--- Pickalot.py
+++ Pickalot.py
@@ -1,51 +1,111 @@
-'''
-Author: Sam
-
-'''
-
 products = ["Apples", "Bananas", "Oranges"]
-item = input("Desired product:")
-no_of_items = int(input("Number of product: "))
-add_cart = ["Yes", "No"]
-customer_cart = []
-tariff_per_prod = 1.1
-Total_amount = 0
-quantity = 0
+item = input("Search :")
+no_of_apples = 0
+no_of_bananas = 0
+price_apple = 3
+price_banana = 2
+tot_amount_apples = 0
+tot_amount_bananas = 0
+address = 0
+universal_tariff = 1.1
 
-############## Define functions for all products ##############
+
+##################### Iteration function #######################
+def add_to_cart():
+    another_item = input("Search again:")
+    new_num_of_apples = no_of_apples
+    new_num_of_bananas = no_of_bananas
+    global address
+    if another_item == "Apples":
+        print("Price of 1 apple is R3")
+        add_no_of_apples = int(input("How many apples?"))
+        new_num_of_apples += add_no_of_apples
+        print("Now new price for", new_num_of_apples, "apples is ZAR", int((new_num_of_apples * price_apple) * universal_tariff))
+        check_procedure = input("Do you still wish to add more products (Yes / No)?")
+        while check_procedure == "Yes":
+            return add_to_cart()
+        if check_procedure == "No":
+            address = input("Please write street name to address for delivery:")
+            print("Great! Your checkout price for ", new_num_of_apples, "apples is ZAR", int((new_num_of_apples * price_apple) * universal_tariff),"\n Visit banking app to confirm EFT! \n product shall be delivered at",address, "in 2 days \n Thanks and have a good day!" )
+
+    elif another_item == "Bananas":
+        print("Price of 1 banana is R2")
+        add_no_of_bananas = int(input("How many bananas?"))
+        new_num_of_bananas += add_no_of_bananas
+        print("Now new price for", new_num_of_bananas, "bananas is ZAR", int((new_num_of_bananas * price_banana) * universal_tariff))
+        check_procedure = input("Do you still wish to add more products (Yes / No)?")
+        while check_procedure == "Yes":
+            return add_to_cart()
+        if check_procedure == "No":
+            address = input("Please write street name to address for delivery:")
+            print("Great! Your checkout price for ", new_num_of_bananas, "apples is ZAR", int((new_num_of_bananas * price_banana) * universal_tariff),"\n Visit banking app to confirm EFT! \n product shall be delivered at",address, "in 2 days \n Thanks and have a good day!" )
+    exit()
+###################### END OF ITERATION FUNCTION ##############################################
+
+############# FUNCTIONS FOR PRODUCTS ###########################################################
 def apples():
-    customer_cart.append("Apples")
-    print(customer_cart)
-    price_per_apple = 3
-    print("Price for", no_of_items, item, "is ZAR", no_of_items * price_per_apple * tariff_per_prod)
+    print("Price of 1 apple is R3")
+    global no_of_apples
+    global tot_amount_apples
+    global address
+    if item == products[0]:
+        no_of_apples = int(input("How many apples?"))
+        tot_amount_apples = int((no_of_apples * price_apple) * universal_tariff)
+        print("Price for", no_of_apples, "apples is ZAR", tot_amount_apples, )
+        check_procedure_apple = input("Do you wish to add more products (Yes / No)?")
+        while check_procedure_apple == "Yes":
+            return add_to_cart()
+        if check_procedure_apple == "No":
+            address = input("Please write street name to address for delivery:")
+            print("Great! Your checkout price for ", no_of_apples, "Apples is",
+                  tot_amount_apples, "\n Visit banking app to confirm EFT! \n product shall be delivered at",address, "in 2 days \n Thanks and have a good day!")
+    exit()
 
 def bananas():
-    customer_cart.append("Bananas")
-    print(customer_cart)
-    price_per_banana = 3
-    print("Price for", no_of_items, item, "is ZAR", no_of_items * price_per_banana * tariff_per_prod)
+    print("Price of 1 banana is R2")
+    global no_of_bananas
+    global tot_amount_bananas
+    global address
+    if item == products[1]:
+        no_of_bananas = int(input("How many bananas?"))
+        tot_amount_bananas = int((no_of_bananas * price_banana) * universal_tariff)
+        print("Price for", no_of_bananas, "apples is ZAR",tot_amount_bananas )
+        check_procedure_banana = input("Do you wish to add more products (Yes / No)?")
+        while check_procedure_banana == "Yes":
+            return add_to_cart()
+        if check_procedure_banana == "No":
+            address = input("Please write street name to address for delivery:")
+            print("Great! Your checkout price for ", no_of_bananas, "bananas is",
+                  tot_amount_bananas,
+                  "\n Visit banking app to confirm EFT! \n product shall be delivered at", address,
+                  "in 2 days \n Thanks and have a good day!")
+    exit()
 
-def oranges():
-    customer_cart.append("Oranges")
-    print(customer_cart)
-    print("Products in cart include",customer_cart, "Do you want add more?(Yes/No):")
-    while add_cart == "Yes":
+################# END OF PRODUCT FUNCTIONS ######################################################
 
-    price_per_oranges = 4
-    print("Price for", no_of_items, item, "is ZAR", int(no_of_items * price_per_oranges * tariff_per_prod))
 
-########### Loops #######
+############# FUNCTIONS FOR ALL PERMUTATIONS #######################################
+def two_products():
+    if item == "Apples" and item == "Bananas":
+        print("Great! Your checkout price for ", no_of_bananas, "bananas and", no_of_apples, "apples is ZAR",
+              (tot_amount_bananas + tot_amount_apples),
+                  "\n Visit banking app to confirm EFT! \n product shall be delivered at", address,
+                  "in 2 days \n Thanks and have a good day!")
+
+
+
+
+
+
+###################### END OF PERMUTATIONS #########################################
+
+
+#################### Loops to print outcome ########################################
 while item in products:
     if item == "Apples":
         print(apples())
-        break
 
-    elif item == "Bananas" :
+    elif item == "Bananas":
         print(bananas())
-        break
 
-    else:
-        print(oranges())
-        break
-
-print()
+#################3 END OF LOOPS ######################################################         
